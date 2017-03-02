e252e3107ded57eb4e3ececfffbbcb395cde2e1c
Johan Gaub <johangaub@gmail.com>
2017-03-02 17:54:49 +0100

    add 3D menu

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ $file = $page . '.php';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" media="all">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+
+
+
+
+
+
     <title>
         <?php
         switch($page){
--- page/home.php
+++ page/home.php
@@ -46,3 +46,165 @@
         </div>
     </div>
 
+
+
+    <div class="container">
+        <div id="CONCEPT">
+            <div class="row">
+
+                <div class="title2">
+                    <h2>Concept<h2>
+                </div>
+                <div class="paragraphe">
+                    <p>Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nec observare restricte, ne plus reddat quam acceperit; neque enim verendum est, ne quid excidat, aut ne quid in terram defluat, aut ne plus aequo quid in amicitiam congeratur. Cum saepe multa, tum memini domi in hemicyclio sedentem, ut solebat, cum et ego essem una et pauci admodum familiares, in eum sermonem illum incidere qui tum forte multis erat in ore. Meministi enim profecto, Attice, et eo magis, quod P. Sulpicio utebare multum, cum is tribunus plebis capitali odio a Q. Pompeio, qui tum erat consul, dissideret, quocum coniunctissime et amantissime vixerat, quanta esset hominum vel admiratio vel querella.</p>
+                </div>
+
+            </div>
+
+            <section id="three" class="no-padding">
+                <div class="container-fluid">
+                    <div class="row no-gutter">
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/deer.jpg">
+                                <img src="./assets/deer.jpg" class="img-responsive" alt="Image 1">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/beach.jpg">
+                                <img src="./assets/beach.jpg" class="img-responsive" alt="Image 2">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="clearfix hidden-lg"> </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/lake.jpg">
+                                <img src="./assets/lake.jpg" class="img-responsive" alt="Image 3">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/bike.jpg">
+                                <img src="./assets/bike.jpg" class="img-responsive" alt="Image 4">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="clearfix hidden-lg"> </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/city.jpg">
+                                <img src="./assets/city.jpg" class="img-responsive" alt="Image 5">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="./assets/colors.jpg">
+                                <img src="./assets/colors.jpg" class="img-responsive" alt="Image 6">
+                                <div class="gallery-box-caption">
+                                    <div class="gallery-box-content">
+                                        <div>
+                                            <i class="icon-lg ion-ios-search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <!-- Top Navigation -->
+            <div class="codrops-top clearfix">
+
+
+            </div>
+            <div>
+                <h1>Concept</h1>
+            </div>
+            <div class="wrapper">
+                <ul class="stage clearfix">
+
+                    <li class="scene">
+                        <div class="movie" onclick="return true">
+                            <div class="poster"></div>
+                            <div class="info">
+                                <header>
+                                    <h1>It's a Wonderful Life</h1>
+                                    <span class="year">1946</span>
+                                    <span class="rating">PG</span>
+                                    <span class="duration">130 minutes</span>
+                                </header>
+                                <p>
+                                    In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="scene">
+                        <div class="movie" onclick="return true">
+                            <div class="poster"></div>
+                            <div class="info">
+                                <header>
+                                    <h1>Vengeance Valley</h1>
+                                    <span class="year">1951</span>
+                                    <span class="rating">NR</span>
+                                    <span class="duration">83 minutes</span>
+                                </header>
+                                <p>
+                                    A cattle baron takes in an orphaned boy and raises him, causing his own son to resent the boy. As they get older the resentment festers into hatred, and eventually the real son frames his stepbrother for fathering an illegitimate child that is actually his, seeing it as an opportunity to get his half-brother out of the way so he can have his father's empire all to himself.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="scene">
+                        <div class="movie" onclick="return true">
+                            <div class="poster"></div>
+                            <div class="info">
+                                <header>
+                                    <h1>The Gold Rush</h1>
+                                    <span class="year">1925</span>
+                                    <span class="rating">NR</span>
+                                    <span class="duration">95 minutes</span>
+                                </header>
+                                <p>
+                                    The Tramp travels to the Yukon to take part in the Klondike Gold Rush. He gets mixed up with some burly characters and falls in love with the beautiful Georgia. He tries to win her heart with his singular charm.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div><!-- /wrapper -->
+        </div><!-- /container -->
